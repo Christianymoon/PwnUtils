b3bcaebe620d986c5732fff95fc5eb9c5ea24005
Christianymoon <chrisvergara7@outlook.com>
2026-01-24 01:38:17 -0600

    New: Argument parser added for pwnUtils

---
+++ argparse.lua
@@ -0,0 +1,24 @@
+local utils = require('pwnUtils')
+
+argparse = utils.ArgumentParse(arg)
+argparse.setopts("h", "This call helps")
+argparse.setopts("a", "This are doing a parameter")
+argparse.setopts("b", "This are doing b parameter")
+argparse.setopts("c", "This are doing c parameter")
+argparse.setopts("v", "Verbosity")
+
+if argparse.verify("-h") then
+	argparse.help() -- Prints help for the user in the console
+end
+
+if argparse.verify("-c") then -- Verify if is registered and is declared on script
+	print("Execute -c option successfully")
+end
+
+if argparse.verify("-b") then
+	print("Execute -b option successfully")
+end
+
+if argparse.verify("-v") then
+	print("Verbosity habilited")
+end
--- pwnUtils.lua
+++ pwnUtils.lua
@@ -64,6 +64,53 @@ function pwnUtils.newProgressBar(line, name)
 	}
 end
 
+
+function pwnUtils.ArgumentParse(args)
+	local parseOptions = {}
+	local args = args
+
+	function getHelp()
+		io.write("Get Help using -h \n")
+		for i, opt in ipairs(parseOptions) do
+			io.write(string.format("[%d] Option: %s - %s\n", i, opt.option, opt.help))
+		end
+	end
+
+	function verifyOpt(val)
+		for i, arg in ipairs(args) do
+			if arg == val then
+				for j, opt in ipairs(parseOptions) do
+					if opt.option == val then
+						return true
+					end
+				end
+			end
+		end
+		return false
+	end
+
+	return {
+		setopts = function(letter, help)
+			table.insert(parseOptions, { option = string.format('-%s', letter), help = help})
+		end,
+		getopts = function()
+			return parseOptions
+		end,
+		setargs = function(args)
+			args = args
+		end,
+		getargs = function()
+			return args 
+		end,
+		verify = function(opt)
+			return verifyOpt(opt)
+		end,
+		help = function()
+			getHelp()
+		end
+	}
+end
+
 return pwnUtils
 
 
